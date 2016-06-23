8327c27e94a38e6446c5b5a748aaaf107594aac8
Lou <lfranklin@deloitte.co.uk>
2016-06-23 20:41:00 +0100

    basic form wired up

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,25 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
-const RootUrl = 'http://reduxblog.herokuapp.com/api';
+export const CREATE_POST = 'CREATE_POST';
+
+const RootUrl = 'http://reduxblog.herokuapp.com/api/posts';
 const API_Key = '?key=remain';
 
 export function fetchPosts() {
-  const request = axios.get(`${RootUrl}/posts${API_Key}`);
+  const request = axios.get(`${RootUrl}${API_Key}`);
 
   return {
     type: FETCH_POSTS,
+    payload: request
+  }
+}
+
+export function createPost(props) {
+  const request = axios.post(`${RootUrl}${API_Key}`, props);
+
+  return {
+    type: CREATE_POST,
     payload: request
   }
 }
